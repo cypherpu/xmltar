2d6fec7ed89fecaef665211078573a8841bd5f28
dbetz <dbetz@localhost>
2019-01-16 20:17:01 -0800

    Adding pre-build.sh.

--- pre-build.sh
+++ pre-build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-#/usr/bin/rsync --checksum  -r ../Bidirectional_Pipe/src/common/Bidirectional_Pipe src/common
+/usr/bin/rsync --checksum  -r ../Bidirectional_Pipe/src/common/Bidirectional_Pipe/ src/common/Generated
