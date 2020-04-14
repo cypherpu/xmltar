73406ae2f29c412dbb7ae6fc91b09ebd46322084
dbetz <dbetz@localhost>
2020-04-14 12:48:01 -0700

    Add copyright and license to several files.

--- src/common/Snapshot/SnapshotEvent.cpp
+++ src/common/Snapshot/SnapshotEvent.cpp
@@ -1,8 +1,26 @@
 /*
- * SnapshotEvent.cpp
- *
- *  Created on: Mar 29, 2019
- *      Author: dbetz
+
+SnapshotEvent.cpp
+Copyright 2019-2020 David A. Betz
+David.Betz.MD@gmail.com
+Created on: Mar 29, 2019
+Author: dbetz
+
+This file is part of Xmltar.
+
+Xmltar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Xmltar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Xmltar.  If not, see <https://www.gnu.org/licenses/>.
+
  */
 
 #include <ostream>
--- src/common/Snapshot/SnapshotFileEntry.hpp
+++ src/common/Snapshot/SnapshotFileEntry.hpp
@@ -1,8 +1,26 @@
 /*
- * SnapshotFileEntry.hpp
- *
- *  Created on: Nov 4, 2017
- *      Author: dbetz
+
+SnapshotFileEntry.hpp
+Copyright 2017-2020 David A. Betz
+David.Betz.MD@gmail.com
+Created on: Nov 4, 2017
+Author: dbetz
+
+This file is part of Xmltar.
+
+Xmltar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Xmltar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Xmltar.  If not, see <https://www.gnu.org/licenses/>.
+
  */
 
 #ifndef SRC_COMMON_SNAPSHOT_SNAPSHOTFILEENTRY_HPP_
