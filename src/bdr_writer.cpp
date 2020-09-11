3062e171a1f540fa75952a6dbc2c7780543ddaec
dbetz <dbetz@localhost>
2020-09-11 05:54:11 -0700

    Start BDR discovery.

--- src/bdr_writer.cpp
+++ src/bdr_writer.cpp
@@ -5,6 +5,23 @@
  *      Author: dbetz
  */
 
-int main(int argc, char *argv[]){
+#include <sstream>
 
+#include "Process/Process.hpp"
+#include "Process/Connection.hpp"
+#include "Process/Utilities.hpp"
+
+void IdentifyBDRs(){
+	std::istringstream iss;
+	std::ostringstream oss;
+
+	Process scanbus("/usr/local/bin/cdrecord",{"cdrecord","-scanbus"},"cdrecord");
+	Chain1(scanbus,iss,oss);
+
+	std::cerr << oss.str() << std::endl;
+}
+
+
+int main(int argc, char *argv[]){
+	IdentifyBDRs();
 }
