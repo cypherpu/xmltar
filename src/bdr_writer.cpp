18d707337247a2f291eeee48cf848fe697d7524e
dbetz <dbetz@localhost>
2020-09-12 16:00:03 -0700

    Added support for passphrase stored in json file.

--- src/bdr_writer.cpp
+++ src/bdr_writer.cpp
@@ -5,23 +5,96 @@
  *      Author: dbetz
  */
 
+#include <map>
 #include <sstream>
+
+#include <json.hpp>
 
 #include "Process/Process.hpp"
 #include "Process/Connection.hpp"
 #include "Process/Utilities.hpp"
 
-void IdentifyBDRs(){
-	std::istringstream iss;
-	std::ostringstream oss;
+class Device {
+public:
+	int scsibus_;
+	int target_;
+	int lun_;
+
+	Device()
+		: scsibus_(-1), target_(-1), lun_(-1){}
+
+	Device(int scsibus, int target, int lun)
+		: scsibus_(scsibus), target_(target), lun_(lun){}
+};
+
+std::map<std::string,Device> IdentifyBDRs(){
+	std::map<std::string,Device> result;
+
+	std::istringstream iss1;
+	std::ostringstream oss1;
 
 	Process scanbus("/usr/local/bin/cdrecord",{"cdrecord","-scanbus"},"cdrecord");
-	Chain1(scanbus,iss,oss);
+	Chain1(scanbus,iss1,oss1);
 
-	std::cerr << oss.str() << std::endl;
+	std::istringstream inputLines1(oss1.str());
+	for(std::string line1; std::getline(inputLines1,line1); ){
+		if (line1.find("BD-RE")!=std::string::npos && line1.find("Removable CD-ROM")!=std::string::npos){
+			std::istringstream inputLine1(line1);
+			int scsibus, target, lun;
+			char c;
+			inputLine1 >> scsibus >> c >> target >> c >> lun;
+			std::cerr << "scsibus=" << scsibus << " target=" << target << " lun=" << lun << std::endl;
+
+			std::ostringstream device;
+			device << "dev=" << scsibus << "," << target << "," << lun;
+			Process prcap("/usr/local/bin/cdrecord",{"cdrecord",device.str().c_str(),"-prcap"},"cdrecord");
+			std::istringstream iss2;
+			std::ostringstream oss2;
+			Chain1(prcap,iss2,oss2);
+
+			std::istringstream inputLines2(oss2.str());
+			for(std::string line2; std::getline(inputLines2,line2); ){
+				if (line2.find("Feature: 'Logical Unit Serial Number' (current) (persistent)\tSerial: '")!=std::string::npos){
+					std::istringstream iss3(line2);
+					std::string discard;
+					std::string logicalUnitSerialNumber;
+					iss3 >> discard >> discard >> discard >> discard >> discard >> discard >> discard >> discard >> logicalUnitSerialNumber;
+
+					if (logicalUnitSerialNumber[0]=='\'') logicalUnitSerialNumber.erase(0,1);
+					if (logicalUnitSerialNumber.back()=='\'') logicalUnitSerialNumber.erase(logicalUnitSerialNumber.length()-1,1);
+					std::cerr << logicalUnitSerialNumber << std::endl;
+
+					result[logicalUnitSerialNumber]=Device(scsibus,target,lun);
+				}
+			}
+		}
+	}
+
+	return result;
 }
 
 
 int main(int argc, char *argv[]){
-	IdentifyBDRs();
+	std::vector<std::string> orderedSerialNumbers {
+		"M64IB9I0842",
+		"M6IIB9H5304",
+		"M62IB9I0856",
+		"M66IA3A1811",
+		"M66IA395830",
+		"M6BIA394645",
+		"M69IA3H2850",
+		"M6JIA3H2107"
+	};
+
+	std::map<std::string,Device> serialToScsi=IdentifyBDRs();
+
+	std::ifstream ifs("/home/dbetz/git/Private/xmltar.json");
+	auto j=nlohmann::json::parse(ifs);
+
+
+	std::cerr << "TeachGoogleClientId=\"" << teachGoogleClientId << "\"" << std::endl;
+	std::cerr << "TeachGoogleClientSecret=\"" << teachGoogleClientSecret << "\"" << std::endl;
+	std::cerr << "TeachGoogleCallback=\"" << teachGoogleCallback << "\"" << std::endl;
+	std::cerr << "ServerDomainAndPort=\"" << serverDomainAndPort << "\"" << std::endl;
+
 }
--- src/xmltar.cpp
+++ src/xmltar.cpp
@@ -30,6 +30,8 @@ along with Xmltar.  If not, see <https://www.gnu.org/licenses/>.
 #include <termios.h>
 #include <unistd.h>
 
+#include <json.hpp>
+
 #include <boost/format.hpp>
 #include <spdlog/spdlog.h>
 
@@ -50,91 +52,13 @@ int main(int argc, char const *argv[])
 	XmltarGlobals globals;
 
 	try {
-#if 0
-	    termios oldt;
-	    tcgetattr(STDIN_FILENO, &oldt);
-	    termios newt = oldt;
-	    newt.c_lflag &= ~ECHO;
-	    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+		std::ifstream ifs("/home/dbetz/git/Private/xmltar.json");
+		auto j=nlohmann::json::parse(ifs);
+		globals.passphrase_=j["passphrase"];
 
-	    std::string s;
-	    std::cout << "Before getline" << std::endl;
-	    std::getline(std::cin, s);
-	    std::cout << "After getline" << std::endl;
+		std::cerr << "passphrase=\"" << globals.passphrase_ << "\""
 
-	    std::cout << s << std::endl;
-
-	    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-
-
-	    //Key and IV setup
-	    //AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-
-	    //bit). This key is secretly exchanged between two parties before communication
-	    //begins. DEFAULT_KEYLENGTH= 16 bytes
-	    CryptoPP::byte key[ CryptoPP::AES::DEFAULT_KEYLENGTH ], iv[ CryptoPP::AES::BLOCKSIZE ];
-	    memset( key, 0x00, CryptoPP::AES::DEFAULT_KEYLENGTH );
-	    memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE );
-
-	    //
-	    // String and Sink setup
-	    //
-	    std::string plaintext = "Now is the time for all good men to come to the aide...";
-	    std::string ciphertext;
-	    std::string decryptedtext;
-
-	    //
-	    // Dump Plain Text
-	    //
-	    std::cout << "Plain Text (" << plaintext.size() << " bytes)" << std::endl;
-	    std::cout << plaintext;
-	    std::cout << std::endl << std::endl;
-
-	    //
-	    // Create Cipher Text
-	    //
-	    CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
-	    CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption( aesEncryption, iv );
-
-	    CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink( ciphertext ) );
-	    stfEncryptor.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() );
-	    stfEncryptor.MessageEnd();
-
-	    //
-	    // Dump Cipher Text
-	    //
-	    std::cout << "Cipher Text (" << ciphertext.size() << " bytes)" << std::endl;
-
-	    for( int i = 0; i < ciphertext.size(); i++ ) {
-
-	        std::cout << "0x" << std::hex << (0xFF & static_cast<CryptoPP::byte>(ciphertext[i])) << " ";
-	    }
-
-	    std::cout << std::endl << std::endl;
-
-	    //
-	    // Decrypt
-	    //
-	    CryptoPP::AES::Decryption aesDecryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
-	    CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption( aesDecryption, iv );
-
-	    CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink( decryptedtext ) );
-	    stfDecryptor.Put( reinterpret_cast<const unsigned char*>( ciphertext.c_str() ), ciphertext.size() );
-	    stfDecryptor.MessageEnd();
-
-	    //
-	    // Dump Decrypted Text
-	    //
-	    std::cout << "Decrypted Text: " << std::endl;
-	    std::cout << decryptedtext;
-	    std::cout << std::endl << std::endl;
-
-	    return 0;
-#endif
-
-	    globals.passphrase_="The quick brown fox jumps over t";
 	    globals.key_=globals.KeyFromPassphrase(globals.passphrase_);
-	    globals.key_="The quick brown fox jumps over t";
 
 	    spdlog::debug("Before XmltarOptions");
 		globals.options_.ProcessOptions(argc, argv);
