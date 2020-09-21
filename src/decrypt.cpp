7823ff9720a8ae8d67efd92b895bc1fe737d6f0f
dbetz <dbetz@localhost>
2020-09-21 03:05:00 -0700

    Begin work on standalone archive decryption utility.

---
+++ src/decrypt.cpp
@@ -0,0 +1,27 @@
+/*
+
+decrypt.cpp
+Copyright 2020 by David A. Betz
+David.Betz.MD@gmail.com
+Created on: Sep 21, 2020
+Author: dbetz
+
+This file is part of Assess.
+
+Assess is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Assess is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Assess.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+
+
