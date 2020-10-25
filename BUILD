540f764b6bd10f0011560bf970f3532eaef227f0
dbetz <dbetz@localhost>
2020-10-25 05:22:01 -0700

    Implement working random_file_tree.

--- BUILD
+++ BUILD
@@ -5,6 +5,36 @@
 # nautilus graph.png
 
 cc_binary(
+	name = "random_file_tree",
+	srcs = ["src/random_file_tree.cpp"]
+			+glob(["src/common/**/*.cpp"] )
+			+glob(["src/common/**/*.hpp"])
+			,
+	includes = [],
+	copts = ["-g","-std=c++2a","-Isrc","-Isrc/common","-Isrc/common/Generated"],
+	linkopts = [
+				"-lgtest",
+				"-lzstd",
+				"-lz",
+				"-lxxhash",
+				"-lboost_system",
+				"-lxerces-c",
+				"-lexpat",
+				"-lboost_date_time",
+				"-lcryptopp",
+				"-lpthread",
+				"-L/usr/lib/gcc/x86_64-redhat-linux/8/",
+				"-lstdc++fs",
+				"-lcrypto",
+				"-lsodium",
+				"-licui18n",
+				"-licuio",
+				"-licutu",
+				"-licuuc",
+				]
+)
+
+cc_binary(
 	name = "Xmltar.Test",
 	srcs = ["src/Xmltar.Test.cpp"]
 			+glob(["src/common/**/*.cpp"] )
--- src/random_file_tree.cpp
+++ src/random_file_tree.cpp
@@ -1,25 +1,25 @@
 /*
 
-random_files.cpp
+random_file_tree.cpp
 Copyright 2020 by David A. Betz
 David.Betz.MD@gmail.com
 Created on: Sep 28, 2020
 Author: dbetz
 
-This file is part of Assess.
+This file is part of xmltar.
 
-Assess is free software: you can redistribute it and/or modify
+xmltar is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Assess is distributed in the hope that it will be useful,
+xmltar is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Assess.  If not, see <https://www.gnu.org/licenses/>.
+along with xmltar.  If not, see <https://www.gnu.org/licenses/>.
 
 */
 
@@ -28,8 +28,10 @@ along with Assess.  If not, see <https://www.gnu.org/licenses/>.
 #include <filesystem>
 #include <random>
 #include <stdexcept>
+#include <vector>
 
 #include <unicode/ustring.h>
+#include <unicode/uchar.h>
 
 #include "Utilities/Options-TarStyle.hpp"
 #include "Utilities/Crypto/Encryptor.hpp"
@@ -38,46 +40,43 @@ along with Assess.  If not, see <https://www.gnu.org/licenses/>.
 #include "Utilities/Crypto/DecryptIdentity.hpp"
 #include "Compressors/Compressor.hpp"
 
-/*
- * 		u_strFromUTF8
- *
- */
 template<typename RNG>
-std::vector<UChar> RandomUTF8Character(RNG & rng){
+std::string RandomUTF8Character(RNG & rng){
 	std::uniform_int_distribution<unsigned int> codePoints(0,0x10ffff);
 
 	size_t codePoint;
-	std::vector<char> utf8CodePoint;
-	int32_t pDestLength;
-	UErrorCode pErrorCode;
+	std::string utf8CodePoint;
+	UErrorCode errorCode;
+	int32_t unicodeLength;
 
 	do {
 		codePoint=codePoints(rng);
 
+		//std::cerr << "codePoint=" << std::hex << codePoint << std::endl;
 		utf8CodePoint.clear();
 		if (0x00<=codePoint && codePoint<=0x7f){
 			char c=codePoint;
 			utf8CodePoint.push_back(c);
 		}
 		else if (codePoint<=0x07ff){
-			char c1=0x1f & (codePoint>>6);
-			char c2=0x3f & codePoint;
+			char c1=0xc0 | (0x1f & (codePoint>>6));
+			char c2=0x80 | (0x3f & codePoint);
 			utf8CodePoint.push_back(c1);
 			utf8CodePoint.push_back(c2);
 		}
 		else if (codePoint<=0xffff){
-			char c1=0x0f & (codePoint>>12);
-			char c2=0x3f & (codePoint >> 6);
-			char c3=0x3f & codePoint;
+			char c1=0xe0 | (0x0f & (codePoint>>12));
+			char c2=0x80 | (0x3f & (codePoint >> 6));
+			char c3=0x80 | (0x3f & codePoint);
 			utf8CodePoint.push_back(c1);
 			utf8CodePoint.push_back(c2);
 			utf8CodePoint.push_back(c3);
 		}
 		else if (codePoint<=0x10ffff){
-			char c1=0x03 & (codePoint>>18);
-			char c2=0x3f & (codePoint>>12);
-			char c3=0x3f & (codePoint>>6);
-			char c4=0x3f & codePoint;
+			char c1=0xf0 | (0x03 & (codePoint>>18));
+			char c2=0x80 | (0x3f & (codePoint>>12));
+			char c3=0x80 | (0x3f & (codePoint>>6));
+			char c4=0x80 | (0x3f & codePoint);
 			utf8CodePoint.push_back(c1);
 			utf8CodePoint.push_back(c2);
 			utf8CodePoint.push_back(c3);
@@ -85,11 +84,136 @@ std::vector<UChar> RandomUTF8Character(RNG & rng){
 		}
 		else throw std::logic_error("RandomUTF8Character: invalid code point");
 
-		pErrorCode=U_ZERO_ERROR;
-		u_strFromUTF8(NULL,0,&pDestLength,utf8CodePoint.data(),utf8CodePoint.size(),&pErrorCode);
-	} while(U_FAILURE(pErrorCode) || pDestLength!=1);
+		//for(int i=0; i<utf8CodePoint.size(); ++i)
+		//	std::cerr << "utf8CodePoint[" << i << "]=" << std::hex << (int) (unsigned char) utf8CodePoint[i] << std::endl;
+
+		UChar buf[9];
+		int32_t destLength;
+		errorCode=U_ZERO_ERROR;
+		u_strFromUTF8(buf,5,&destLength,utf8CodePoint.data(),utf8CodePoint.size(),&errorCode);
+		if (U_FAILURE(errorCode)){
+			UCharCategory cat=(UCharCategory) u_charType(codePoint);
+			std::cerr << "\t\t\t";
+			switch(cat){
+			case U_UNASSIGNED:
+				std::cerr << "U_UNASSIGNED" << std::endl;
+				break;
+			case U_UPPERCASE_LETTER:
+				std::cerr << "U_UPPERCASE_LETTER" << std::endl;
+				break;
+			case U_LOWERCASE_LETTER:
+				std::cerr << "U_LOWERCASE_LETTER" << std::endl;
+				break;
+			case U_TITLECASE_LETTER:
+				std::cerr << "U_TITLECASE_LETTER" << std::endl;
+				break;
+			case U_MODIFIER_LETTER:
+				std::cerr << "U_MODIFIER_LETTER" << std::endl;
+				break;
+			case U_OTHER_LETTER:
+				std::cerr << "U_OTHER_LETTER" << std::endl;
+				break;
+			case U_NON_SPACING_MARK:
+				std::cerr << "U_NON_SPACING_MARK" << std::endl;
+				break;
+			case U_ENCLOSING_MARK:
+				std::cerr << "U_ENCLOSING_MARK" << std::endl;
+				break;
+			case U_COMBINING_SPACING_MARK:
+				std::cerr << "U_COMBINING_SPACING_MARK" << std::endl;
+				break;
+			case U_DECIMAL_DIGIT_NUMBER:
+				std::cerr << "U_DECIMAL_DIGIT_NUMBER" << std::endl;
+				break;
+			case U_LETTER_NUMBER:
+				std::cerr << "U_LETTER_NUMBER" << std::endl;
+				break;
+			case U_OTHER_NUMBER:
+				std::cerr << "U_OTHER_NUMBER" << std::endl;
+				break;
+			case U_SPACE_SEPARATOR:
+				std::cerr << "U_SPACE_SEPARATOR" << std::endl;
+				break;
+			case U_LINE_SEPARATOR:
+				std::cerr << "U_LINE_SEPARATOR" << std::endl;
+				break;
+			case U_PARAGRAPH_SEPARATOR:
+				std::cerr << "U_PARAGRAPH_SEPARATOR" << std::endl;
+				break;
+			case U_CONTROL_CHAR:
+				std::cerr << "U_CONTROL_CHAR" << std::endl;
+				break;
+			case U_FORMAT_CHAR:
+				std::cerr << "U_FORMAT_CHAR" << std::endl;
+				break;
+			case U_PRIVATE_USE_CHAR:
+				std::cerr << "U_PRIVATE_USE_CHAR" << std::endl;
+				break;
+			case U_SURROGATE:
+				std::cerr << "U_SURROGATE" << std::endl;
+				break;
+			case U_DASH_PUNCTUATION:
+				std::cerr << "U_DASH_PUNCTUATION" << std::endl;
+				break;
+			case U_START_PUNCTUATION:
+				std::cerr << "U_START_PUNCTUATION" << std::endl;
+				break;
+			case U_END_PUNCTUATION:
+				std::cerr << "U_END_PUNCTUATION" << std::endl;
+				break;
+			case U_CONNECTOR_PUNCTUATION:
+				std::cerr << "U_CONNECTOR_PUNCTUATION" << std::endl;
+				break;
+			case U_OTHER_PUNCTUATION:
+				std::cerr << "U_OTHER_PUNCTUATION" << std::endl;
+				break;
+			case U_MATH_SYMBOL:
+				std::cerr << "U_MATH_SYMBOL" << std::endl;
+				break;
+			case U_CURRENCY_SYMBOL:
+				std::cerr << "U_CURRENCY_SYMBOL" << std::endl;
+				break;
+			case U_MODIFIER_SYMBOL:
+				std::cerr << "U_MODIFIER_SYMBOL" << std::endl;
+				break;
+			case U_OTHER_SYMBOL:
+				std::cerr << "U_OTHER_SYMBOL" << std::endl;
+				break;
+			case U_INITIAL_PUNCTUATION:
+				std::cerr << "U_INITIAL_PUNCTUATION" << std::endl;
+				break;
+			case U_FINAL_PUNCTUATION:
+				std::cerr << "U_FINAL_PUNCTUATION" << std::endl;
+				break;
+			case U_CHAR_CATEGORY_COUNT:
+				std::cerr << "U_CHAR_CATEGORY_COUNT" << std::endl;
+				break;
+			default:
+				std::cerr << "Unknown category!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+				break;
+			}
+			continue;
+		}
+		unicodeLength=u_countChar32(buf,destLength);
+		// std::cerr << (int) errorCode << " " << (int) U_ZERO_ERROR << "\"" << std::boolalpha << (int) U_FAILURE(errorCode) << "\" " << destLength << " " << unicodeLength << std::endl;
+	} while(U_FAILURE(errorCode) || unicodeLength!=1);
 
 	return utf8CodePoint;
+}
+
+
+template<typename RNG>
+std::string RandomUTF8Filename(int filenameLength, RNG & rng){
+	std::string filename;
+
+	for( ; ; ){
+		filename.clear();
+		for(int i=0; i<filenameLength; ++i)
+			filename+=RandomUTF8Character(rng);
+		if (filename!="/") break;
+	}
+
+	return filename;
 }
 
 class RandomFileTreeOptions {
@@ -138,6 +262,8 @@ public:
     			p.Assign_Args(log10MinFileSize_));
     	p.Add_Option(Parse_Opts::ARGS_1,"","--max-file-size","log10 of the maximum file size",
     			p.Assign_Args(log10MaxFileSize_));
+
+    	p.Parse(argc, argv);
     }
 };
 
@@ -150,6 +276,8 @@ public:
 	std::uniform_int_distribution<unsigned int> filesInDirectory_;
 	std::uniform_int_distribution<unsigned int> filenameLength_;
 	std::uniform_real_distribution<double> log10FileSize_;
+
+	std::uniform_int_distribution<uint8_t> randomBytes_;;
 
 	RandomFileTree(int argc, char const *argv[]){
 		options_.ProcessOptions(argc,argv);
@@ -168,7 +296,7 @@ public:
 		mt_.seed(seed);
 
 		{
-			std::uniform_int_distribution<unsigned int>::param_type params(options_.minFilesPerDirectory_,options_.minFilesPerDirectory_);
+			std::uniform_int_distribution<unsigned int>::param_type params(options_.minFilesPerDirectory_,options_.maxFilesPerDirectory_);
 			filesInDirectory_.param(params);
 		}
 		{
@@ -179,9 +307,14 @@ public:
 			std::uniform_real_distribution<double>::param_type params(options_.log10MinFileSize_,options_.log10MaxFileSize_);
 			log10FileSize_.param(params);
 		}
+		{
+			std::uniform_int_distribution<uint8_t>::param_type params(0,255);
+			randomBytes_.param(params);
+		}
 	}
 
-	void PopulateDirectory(int depth){
+	void PopulateDirectory(std::filesystem::path baseDir, int depth){
+		++depth;
 		unsigned int nDirs=options_.branchingFactor_;
 		unsigned int nFiles=filesInDirectory_(mt_);
 
@@ -194,17 +327,40 @@ public:
 		std::shuffle(fileTypes.begin(),fileTypes.end(),mt_);
 
 		for(auto i : fileTypes){
-			if (*i==std::filesystem::file_type::directory){
+			if (i==std::filesystem::file_type::directory){
+				std::cerr << "Directory" << std::endl;
+				std::filesystem::path newDir;
+				for( ; ; ){
+					unsigned int nFilenameLength=filenameLength_(mt_);
+					std::cerr << "nFilenameLength=" << nFilenameLength << std::endl;
+					newDir=baseDir / RandomUTF8Filename(nFilenameLength,mt_);
+					std::cerr << "newDir=" << newDir << std::endl;
+					if (std::filesystem::exists(newDir)) continue;
+					if (std::filesystem::create_directory(newDir,baseDir)) break;
+				}
 
+				if (depth<options_.maxDepth_) PopulateDirectory(newDir, depth);
 			}
-			else if (*i==std::filesystem::file_type::regular){
-
+			else if (i==std::filesystem::file_type::regular){
+				std::cerr << "File" << std::endl;
+				size_t nFilesize=std::floor(std::pow(10,log10FileSize_(mt_)));
+				std::filesystem::path newFile;
+				for( ; ; ){
+					unsigned int nFilenameLength=filenameLength_(mt_);
+					std::cerr << "nFilenameLength=" << nFilenameLength << std::endl;
+					newFile=baseDir / RandomUTF8Filename(nFilenameLength,mt_);
+					if (std::filesystem::exists(newFile)) continue;
+					std::cerr << "newFile=" << newFile << std::endl;
+					std::ofstream ofs(newFile.c_str());
+					if (ofs){
+						for(size_t i=0; i<nFilesize; ++i)
+							ofs << randomBytes_(mt_);
+						break;
+					}
+					ofs.close();
+				}
 			}
 		}
-
-		unsigned int nFilenameLength=filenameLength_(mt_);
-		unsigned int nFilesize=std::floor(std::pow(10,log10FileSize_(mt_)));
-
 	}
 };
 
@@ -218,5 +374,12 @@ int main(int argc, char const *argv[]){
 	else if (!std::filesystem::is_directory(std::filesystem::symlink_status(randomFileTree.options_.baseDir_)))
 		throw std::runtime_error("random_file_tree: "+randomFileTree.options_.baseDir_.string()+" is not a directory");
 
-	randomFileTree.PopulateDirectory(0);
+	randomFileTree.PopulateDirectory(randomFileTree.options_.baseDir_,0);
+
+    for(auto& p: std::filesystem::recursive_directory_iterator(randomFileTree.options_.baseDir_)){
+    	for(auto c : p.path().string())
+    		std::cerr << std::hex << (int)(unsigned char) c << " ";
+    	std::cerr << std::endl;
+    	std::cerr << p.path() << std::endl;
+    }
 }
